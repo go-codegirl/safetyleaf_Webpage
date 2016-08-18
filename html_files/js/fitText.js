80a8c530c82ff98038687ed24dfa027f8e181287
Sravani Marupeddi <SravaniMarupeddi@Karthiks-MacBook-Pro.local>
2016-08-17 21:12:29 -0500

    index Changes

---
+++ html_files/js/fitText.js
@@ -0,0 +1,7 @@
+/**
+ * Created by SravaniMarupeddi on 8/15/16.
+ */
+$("#fittext1").fitText();
+$("#fittext2").fitText(7);
+//    $("#fittext2").fitText(1.2);
+$("#fittext1").fitText(1.2, { minFontSize: '40px', maxFontSize: '60px' });
